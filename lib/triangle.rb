34f9bf5a4a50a19c47659658e91ad9bd73628f44
Sahnun Mohamud <shmohamud@Sahnuns-MBP.fios-router.home>
2020-11-21 21:32:44 -0500

    ruby refresher

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+    is_triangle?
+  end
+
+  def kind 
+    if((@a == @b && @b != @c) || (@a == @c && @c != @b) || (@b==@c && @c != @a))
+      return :isosceles
+    end
+    if(@a == @b && @b == @c)
+      return :equilateral
+    end    
+     if(@a != @b && @b != @c)
+    return :scalene
+  end 
 end
+
+def is_triangle?
+  if(@a+@b <= @c || @a+@c <= @b || @a + @c <= @b || @b + @c <= @a)
+    raise TriangleError
+  end
+end 
+class TriangleError < StandardError
+end
+end
+
+
+
+
